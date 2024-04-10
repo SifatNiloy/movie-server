4fbecd42ef6b0d4efd0d6817aaf12ed9e674b982
SifatNiloy <sifatniloy18@gmail.com>
2024-04-10 15:36:42 +0600

    product api post done

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
-export function GET(request: NextRequest){
+import prisma from "@/prisma/client";
+export async function GET(request: NextRequest){
+    const products= await prisma.product.findMany()
     return NextResponse.json([
         {id:1, name: 'Milk', price: 4.5},
         {id:2, name: 'Bread', price: 3.5},
@@ -13,5 +15,11 @@ export async function POST(request: NextRequest){
     if(!validation.success){
         return NextResponse.json(validation.error.errors,{status: 400})
     }
-    return NextResponse.json({id:10, name: body.name, price: body.price},{status: 201})
+    const newProduct= await prisma.product.create({
+        data:{
+            name:  body.name,
+            price: body.price,
+        }
+    })
+    return NextResponse.json(newProduct ,{status: 201})
 }
