7c2436a0c07027783ecaeff1d82d5bf9d9dfee5f
SifatNiloy <sifatniloy18@gmail.com>
2024-04-12 17:58:12 +0600

    private route added

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <Link href="/" className='mr-5'>Next.js</Link>
         <Link href="/users" className='mr-5'>users</Link> 
         {/* {status==='loading' && <div><h1>Loading...</h1></div>} */}
-        {status==='authenticated' && <div>{session.user?.name}</div>}
+        {status==='authenticated' && <div>{session.user?.name} <Link href="api/auth/signout" className='ml-3' >Sign Out</Link> </div>}
         {status==='unauthenticated' && <Link href="/api/auth/signin" className='mr-5'>Login</Link>}
     </div>
   )
--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,15 +1,18 @@
 
 import nextAuth from "next-auth";
 import  GoogleProvider from 'next-auth/providers/google';
-
-const handler= nextAuth({
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
+export const authOptions= {
+    adapter: PrismaAdapter(prisma),
     providers:[
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
     ]
-});
+}
+const handler= nextAuth(authOptions);
 
 export {handler as GET, handler as POST}
 
