1d5a0445726bd43d316e2b0a82ff2bf7d2dacc49
SifatNiloy <sifatniloy18@gmail.com>
2024-04-13 12:36:15 +0600

    image component in nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,17 @@ import Link from 'next/link'
 import ProductCard from './components/ProductCard'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
+import coffee from '@/public/images/coffee.jpg'
+
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user?.name }</span>}</h1>
+    <main className='relative h-screen'>
+      {/* <h1>Hello {session && <span>{session.user?.name }</span>}</h1>
       <Link href="/users">users</Link>
-      <ProductCard/>
-      </main>
+      <ProductCard/> */}
+      <Image src={coffee} alt='' fill className='object-cover' sizes="(max-width:480px)100vw , (max-width:768px) 50vw, 33vw" quality={75} priority />
+    </main>
   )
 }
